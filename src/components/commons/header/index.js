6a6485f6d9a63cd7ea9346988c1e393405998c25
Faizan-Khalid99 <86584248+Faizan-Khalid99@users.noreply.github.com>
2022-05-26 19:10:38 +0500

    header and hero section -> styling and position of card to be placed done

--- src/components/commons/header/index.js
+++ src/components/commons/header/index.js
@@ -9,11 +9,15 @@ const Header = () => {
     <>
       <div className="bg-transparent d-flex justify-content-end align-items-center fixed-top">
         <div className="header-position">
-          <h1 className="text-white">WhoDunIt Cats</h1>
+          <h1 className="text-white">WhoDunIt Cats?</h1>
           <div className="d-flex">
             {/* <h6 className="text-white">How it works?</h6>
             <h6 className="text-white">How it works?</h6> */}
-            <h6 className="text-white">How it works?</h6>
+            <h6>
+              <a className="text-white" href="/">
+                How it works?
+              </a>
+            </h6>
           </div>
         </div>
       </div>
--- src/components/landing-page/hero/index.js
+++ src/components/landing-page/hero/index.js
@@ -7,8 +7,8 @@ import "./index.css";
 const HeroSection = () => {
   return (
     <div className="hero">
-      <div className="d-flex">
-        <div className="justify-self-center align-self-center">Hello</div>
+      <div className="container d-flex hero-content justify-content-end  text-white">
+        <div>Card goes here</div>
       </div>
     </div>
   );
