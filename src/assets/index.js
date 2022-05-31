40878b0a5eaf4323f599f32a2944b7d815d411a8
KomailShah05 <syedkomail121@gmail.com>
2022-05-31 14:59:40 +0500

    Home page -> constants added & design done of howItWorks

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
+// imports
 import Caseboard_Colored from "./images/Caseboard_Colored.svg";
+import HowToExport from "./images/how-to-export.svg";
+import Underline from "./images/under-line.svg";
 
-export { Caseboard_Colored };
+// exports
+export { Caseboard_Colored, HowToExport, Underline };
