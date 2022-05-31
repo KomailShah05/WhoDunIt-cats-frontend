be546513824aab3fc60ab0568c9b4dbf3899f502
Faizan-Khalid99 <86584248+Faizan-Khalid99@users.noreply.github.com>
2022-05-31 10:57:52 +0500

    Landing Page and Sass variables -> added rem values instead of pixels and styling in how it works

--- src/components/landing-page/how-it-works/index.js
+++ src/components/landing-page/how-it-works/index.js
@@ -11,28 +11,29 @@ import Underline from "../../../assets/images/under-line.svg";
 const HowItWorks = () => {
   return (
     <section id="HowItWorks">
-      {/* <div className="container"> */}
-      <div className="row justify-content-between">
-        <div className="col-4  align-self-end">
-          <img src={HowToExport} alt="HowToExport" />
-        </div>
-        <div className="col-8 align-self-center ">
-          <h2 className="text-white">How it works?</h2>
-          <img className="underline" src={Underline} alt="Underline" />
-          <p className="working-text">
-            Neque orci dui, sit libero diam magnis nunc et. Molestie diam
-            gravida suspendisse hac aliquam elit. Et turpis placerat non turpis
-            dui. Egestas elit nibh in eros dictum. Odio feugiat amet, diam eget
-            cursus. Nec, vivamus arcu, amet leo aliquam aenean egestas. <br />{" "}
-            Risus elit, et id rhoncus leo lectus nisl molestie consectetur. Mi,
-            suspendisse lectus consectetur at. Aenean dolor, risus, fermentum
-            sem. Nec quam egestas orci, eleifend ac commodo. Leo ornare posuere
-            mauris vestibulum bibendum ante. Nulla at maecenas euismod et
-            consequat nibh. Sit faucibus amet, vitae gravida id lectus quis.
-          </p>
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col-4  d-flex justify-content-center">
+            <img src={HowToExport} alt="HowToExport" />
+          </div>
+          <div className="col-8 align-self-center ">
+            <h2 className="text-white">How it works?</h2>
+            <img className="underline" src={Underline} alt="Underline" />
+            <p className="working-text">
+              Neque orci dui, sit libero diam magnis nunc et. Molestie diam
+              gravida suspendisse hac aliquam elit. Et turpis placerat non
+              turpis dui. Egestas elit nibh in eros dictum. Odio feugiat amet,
+              diam eget cursus. Nec, vivamus arcu, amet leo aliquam aenean
+              egestas. <br /> Risus elit, et id rhoncus leo lectus nisl molestie
+              consectetur. Mi, suspendisse lectus consectetur at. Aenean dolor,
+              risus, fermentum sem. Nec quam egestas orci, eleifend ac commodo.
+              Leo ornare posuere mauris vestibulum bibendum ante. Nulla at
+              maecenas euismod et consequat nibh. Sit faucibus amet, vitae
+              gravida id lectus quis.
+            </p>
+          </div>
         </div>
       </div>
-      {/* </div> */}
     </section>
   );
 };
