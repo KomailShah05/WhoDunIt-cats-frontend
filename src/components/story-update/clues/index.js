4b3967844c671bf36790e61a3e3bf09026affd45
KomailShah05 <syedkomail121@gmail.com>
2022-06-08 16:58:50 +0500

    Story Clue -> Carpusel implemented

--- src/components/story-update/clues/index.js
+++ src/components/story-update/clues/index.js
@@ -1,6 +1,12 @@
 //libraries
 import React from "react";
 
+// components
+import { BlockButton } from "../../commons";
+
+// constants
+import { eng_lang } from "../../../lib/utills/constants";
+
 //styles
 import "./index.scss";
 
@@ -13,43 +19,59 @@ const StoryClues = () => {
       id="carouselExampleControls"
       class="carousel slide carousel__carousel-section"
       data-bs-ride="carousel"
+      data-bs-wrap={false}
+      data-bs-touch={true}
     >
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <h5 className="text-center text-white">Hello komail</h5>
+          <p className="text-center">
+            {eng_lang.story_clues.paragrapgh_line_1}
+            <br />
+            {eng_lang.story_clues.paragrapgh_line_2}
+          </p>
           <img src={HeroImg} class="d-block w-100" alt="..." />
         </div>
         <div class="carousel-item">
-          <h5 className="text-center text-white">Hello komail</h5>
-
+          <p className="text-center">
+            {eng_lang.story_clues.paragrapgh_line_1}
+            <br />
+            {eng_lang.story_clues.paragrapgh_line_2}
+          </p>
           <img src={HeroImg} class="d-block w-100" alt="..." />
         </div>
         <div class="carousel-item">
-          <h5 className="text-center text-white">Hello komail</h5>
-
+          <p className="text-center">
+            {eng_lang.story_clues.paragrapgh_line_1}
+            <br />
+            {eng_lang.story_clues.paragrapgh_line_2}
+          </p>
           <img src={HeroImg} class="d-block w-100" alt="..." />
         </div>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        {/* <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span> */}
-        <button aria-hidden="true">Previous</button>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        {/* <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span> */}
-        <button aria-hidden="true">Next</button>
-      </button>
+      <div className="d-flex justify-content-between carousel__spacing">
+        <BlockButton
+          showImg={false}
+          text={"Previous"}
+          imgPath={""}
+          name={"previous"}
+          handleModalName={""}
+          transparent_btn={true}
+          data_bs_target="#carouselExampleControls"
+          data_bs_slide="prev"
+        />
+        <div className="carousel__min-width">
+          <BlockButton
+            showImg={false}
+            text={"Next"}
+            imgPath={""}
+            name={"next"}
+            handleModalName={""}
+            secondary={false}
+            data_bs_target="#carouselExampleControls"
+            data_bs_slide="next"
+          />
+        </div>
+      </div>
     </div>
   );
 };
