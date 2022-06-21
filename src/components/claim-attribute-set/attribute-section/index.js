a9acac2bbca0ce0bc55afc485e9c714505137f55
faizy99 <faizan.khalid@troontechnologies.com>
2022-06-21 18:55:48 +0500

    Now using compressed pngs provided by Avenson

--- src/components/claim-attribute-set/attribute-section/index.js
+++ src/components/claim-attribute-set/attribute-section/index.js
@@ -16,69 +16,69 @@ import { CatOnePng } from "../../../assets";
 const AttributeSection = () => {
   const selections = eng_lang.claim_attribute_set.section_one.select_data;
   return (
-    <div className="container-xl">
-      <div className="row attribute gx-0 mx-auto">
-        <div className=" col-lg-3 d-flex flex-column align-items-center">
-          <FilterSuspectTrait
-            title={eng_lang.claim_attribute_set.section_one.title}
-            index={1}
-            fill={true}
-          />
-          <div className="attribute__min-width">
-            {selections.map((item) => (
-              <Select heading={item.attribute_name} options={item.options} />
-            ))}
-          </div>
-        </div>
-        <div className="col-lg-6">
-          <FilterSuspectTrait
-            title={eng_lang.claim_attribute_set.section_two.title}
-            index={2}
-            fill={true}
-          />
-          <div className="row w-100 suspects ">
-            <Card />
-          </div>
-        </div>
-        <div className="col-lg-3">
-          <FilterSuspectTrait
-            title={eng_lang.claim_attribute_set.section_three.title}
-            index={3}
-            fill={true}
-          />
-          <div className="d-flex justify-content-center justify-content-lg-start">
-            {" "}
-            <img className="selected-cat" src={CatOnePng} alt="cat" />
-          </div>
-
-          <div className="d-flex justify-content-between metadata-nft">
-            <p className="text-white">
-              {eng_lang.claim_attribute_set.section_three.serial_number}
-            </p>
-            <p className="text-white">
-              {eng_lang.claim_attribute_set.section_three.serial_value}
-            </p>
-          </div>
-          <div className="d-flex  justify-content-between  metadata-nft">
-            <p className="text-white">
-              {eng_lang.claim_attribute_set.section_three.plank_number}
-            </p>
-            <p className="text-white">
-              {eng_lang.claim_attribute_set.section_three.plank_value}
-            </p>
-          </div>
-
-          <button
-            className="btn btn-primary mint-btn"
-            data-mdb-ripple-color="primary"
-            data-bs-toggle="modal"
-            data-bs-target="#smallModalCongrats"
-          >
-            {eng_lang.buttonConstants.mint_suspect}
-          </button>
+    // <div className="container-xl">
+    <div className="row attribute gx-0 mx-auto">
+      <div className=" col-lg-3 d-flex flex-column align-items-center">
+        <FilterSuspectTrait
+          title={eng_lang.claim_attribute_set.section_one.title}
+          index={1}
+          fill={true}
+        />
+        <div className="attribute__min-width">
+          {selections.map((item) => (
+            <Select heading={item.attribute_name} options={item.options} />
+          ))}
         </div>
       </div>
+      <div className="col-lg-6">
+        <FilterSuspectTrait
+          title={eng_lang.claim_attribute_set.section_two.title}
+          index={2}
+          fill={true}
+        />
+        <div className="row w-100 suspects ">
+          <Card />
+        </div>
+      </div>
+      <div className="col-lg-3">
+        <FilterSuspectTrait
+          title={eng_lang.claim_attribute_set.section_three.title}
+          index={3}
+          fill={true}
+        />
+        <div className="d-flex justify-content-center justify-content-lg-start">
+          {" "}
+          <img className="selected-cat" src={CatOnePng} alt="cat" />
+        </div>
+
+        <div className="d-flex justify-content-between metadata-nft">
+          <p className="text-white">
+            {eng_lang.claim_attribute_set.section_three.serial_number}
+          </p>
+          <p className="text-white">
+            {eng_lang.claim_attribute_set.section_three.serial_value}
+          </p>
+        </div>
+        <div className="d-flex  justify-content-between  metadata-nft">
+          <p className="text-white">
+            {eng_lang.claim_attribute_set.section_three.plank_number}
+          </p>
+          <p className="text-white">
+            {eng_lang.claim_attribute_set.section_three.plank_value}
+          </p>
+        </div>
+
+        <button
+          className="btn btn-primary mint-btn"
+          data-mdb-ripple-color="primary"
+          data-bs-toggle="modal"
+          data-bs-target="#smallModalCongrats"
+        >
+          {eng_lang.buttonConstants.mint_suspect}
+        </button>
+      </div>
     </div>
+    // </div>
   );
 };
 
