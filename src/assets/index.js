a2387701161577f76123a4e91a449c84af0b2f4b
faizy99 <faizan.khalid@troontechnologies.com>
2022-06-21 14:51:41 +0500

    Adjusted popup cat images

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,10 +19,15 @@ import underline_vertical from "./images/Undeline-vertical.svg";
 import arrow_down from "./images/Vector-down.svg";
 import HeroImg from "./images/HeroImg.svg";
 import CatOne from "./images/cat-1.svg";
+import CatOnePng from "./images/cat-1.png";
 import CatTwo from "./images/cat-2.svg";
+import CatTwoPng from "./images/cat-2.png";
 import CatThree from "./images/cat-3.svg";
+import CatThreePng from "./images/cat-3.png";
 import CatFour from "./images/cat-4.svg";
+import CatFourPng from "./images/cat-4.png";
 import CatFive from "./images/cat-5.svg";
+import CatFivePng from "./images/cat-5.png";
 import success_1 from "./images/Success_1.png";
 import success_2 from "./images/Success_2.png";
 import success_3 from "./images/success_3.png";
@@ -49,10 +54,15 @@ export {
   arrow_down,
   HeroImg,
   CatOne,
+  CatOnePng,
   CatTwo,
+  CatTwoPng,
   CatThree,
+  CatThreePng,
   CatFour,
+  CatFourPng,
   CatFive,
+  CatFivePng,
   success_1,
   success_2,
   success_3,
