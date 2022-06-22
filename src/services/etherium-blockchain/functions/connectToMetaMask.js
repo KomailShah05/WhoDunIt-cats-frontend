60343c9379030edf6181a2160f46a4c66fca3c22
KomailShah05 <syedkomail121@gmail.com>
2022-06-22 13:33:36 +0500

    Ehtereeum bug fixed

--- src/services/etherium-blockchain/functions/connectToMetaMask.js
+++ src/services/etherium-blockchain/functions/connectToMetaMask.js
@@ -3,7 +3,7 @@ import { blc_func_name } from "../../../lib/utills/constants";
 const connectToMetaMask = () => {
   return new Promise((resolve, reject) => {
     // Asking if metamask is already nt or not
-    if (window.ethereum) {
+    if (window?.ethereum) {
       // res[0] for fetching a first wallet
       window.ethereum
         .request({ method: blc_func_name.ETH_REQUEST_ACCOUNTS })
