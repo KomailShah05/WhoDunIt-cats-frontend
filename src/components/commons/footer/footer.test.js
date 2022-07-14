0ed60e3c6f712d6f40bab549a2c65e2626485997
faizy99 <faizan.khalid@troontechnologies.com>
2022-07-14 11:26:01 +0500

    Added unit testing for links in footer

--- src/components/commons/footer/footer.test.js
+++ src/components/commons/footer/footer.test.js
@@ -1,16 +1,67 @@
 // libraries
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter as Router } from "react-router-dom";
 
 // components
 import Footer from ".";
 
 describe("footer", () => {
-  test(" renders successfully", () => {
+  test(" renders successfully with FAQ Page Link", () => {
     render(
       <Router>
         <Footer />
       </Router>
+    );
+    expect(screen.getByText("FAQ").closest("a")).toHaveAttribute(
+      "href",
+      "/faqs"
+    );
+  });
+
+  test(" renders successfully with Terms and Condition Page Link", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    expect(
+      screen.getByText("Terms and Conditions").closest("a")
+    ).toHaveAttribute("href", "/terms-and-conditions");
+  });
+
+  test(" renders successfully with Twitter Link", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    expect(screen.getByText("Twitter").closest("a")).toHaveAttribute(
+      "href",
+      "/"
+    );
+  });
+
+  test(" renders successfully with Discord Link", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    expect(screen.getByText("Discord").closest("a")).toHaveAttribute(
+      "href",
+      "/"
+    );
+  });
+
+  test(" renders successfully with Instagram Link", () => {
+    render(
+      <Router>
+        <Footer />
+      </Router>
+    );
+    expect(screen.getByText("Instagram").closest("a")).toHaveAttribute(
+      "href",
+      "/"
     );
   });
 });
