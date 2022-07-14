311848bf559bfc46e0cd844c9e3349495923e8e7
KomailShah05 <syedkomail121@gmail.com>
2022-07-14 18:20:58 +0500

    Twitter testing link added

--- src/App.test.js
+++ src/App.test.js
@@ -4,14 +4,19 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(
-    <Provider store={configureStore().store}>
-      <App />
-    </Provider>
-  );
-  expect(screen.getByText("Terms and Conditions").closest("a")).toHaveAttribute(
-    "href",
-    "/terms-and-conditions"
-  );
+describe("App", () => {
+  test("renders learn react link", () => {
+    render(
+      <Provider store={configureStore().store}>
+        <App />
+      </Provider>
+    );
+    expect(
+      screen.getByText("Terms and Conditions").closest("a")
+    ).toHaveAttribute("href", "/terms-and-conditions");
+    expect(screen.getByText("Twitter").closest("a")).toHaveAttribute(
+      "href",
+      "/"
+    );
+  });
 });
