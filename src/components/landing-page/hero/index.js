ec3dd51f02ef4be1073683102a1bada0e037779a
KomailShah05 <syedkomail121@gmail.com>
2022-07-18 15:59:01 +0500

    test cases

--- src/components/landing-page/hero/index.js
+++ src/components/landing-page/hero/index.js
@@ -17,7 +17,6 @@ import { Underline } from "../../../assets";
 const HeroSection = () => {
   const dispatch = useDispatch();
   const { totalMinted, winner } = useContext(landinPageProps);
-  // const url = window.location.pathname;
 
   const handleModalOpen = () => {
     dispatch(showModalAction("conncetToBuy"));
@@ -26,7 +25,10 @@ const HeroSection = () => {
   return (
     <div className="hero">
       <div className="container d-md-flex hero-content justify-content-lg-end justify-content-center  text-white">
-        <div className={`hero-card ${winner?.success === true && "d-none"}`}>
+        <div
+          className={`hero-card ${winner?.success === true && "d-none"}`}
+          data-testid="hero-card"
+        >
           {totalMinted >= eng_lang.totalNoOfMintToken ? (
             <div className="story-release-heading">
               <h1>{eng_lang.storyRelease.heading}</h1>
@@ -36,7 +38,7 @@ const HeroSection = () => {
           <p>{eng_lang.hero.card_text}</p>
 
           {totalMinted >= eng_lang.totalNoOfMintToken ? (
-            <Link to={routes.STORY_INTRO}>
+            <Link to={routes.STORY_INTRO} data-testid="text-grid-item">
               <div className="btn-container position-relative mt-34">
                 <span className="mas hero-mas">
                   {eng_lang.buttonConstants.begin_hunt}
