d44678deeba1748afda76bbca740fa19a2b1a0fe
faizy99 <faizan.khalid@troontechnologies.com>
2022-07-19 12:31:47 +0500

    Added a test for howitworks

---
+++ src/components/landing-page/how-it-works/howItWorks.test.js
@@ -0,0 +1,34 @@
+// libraries
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter as Router } from "react-router-dom";
+
+// components
+import HowItWorks from ".";
+
+// constants
+import { eng_lang } from "../../../lib/utills/constants";
+import configureStore from "../../../redux/store";
+import { landinPageProps } from "../../../pages/landing-page";
+
+describe("How It Works section", () => {
+  test("If the component successfully renders with heading", () => {
+    //Arrange
+    render(
+      <Router>
+        <Provider store={configureStore().store}>
+          <landinPageProps.Provider
+            value={{ totalMinted: 5000, winner: false }}
+          >
+            <HowItWorks />
+          </landinPageProps.Provider>
+        </Provider>
+      </Router>
+    );
+    //   Assert
+    const headingText = screen.getByText(eng_lang.howItWorks.heading, {
+      exact: true,
+    });
+    expect(headingText).toBeInTheDocument();
+  });
+});
